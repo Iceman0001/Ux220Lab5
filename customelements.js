8bb5744baf23b322f1a68b527e3b553c1d2b62ff
Iceman0001 <hirw1701@mylaurier.ca>
2023-11-08 20:51:57 -0500

    add custom e

--- customelements.js
+++ customelements.js
@@ -4,4 +4,20 @@ class AyanElement extends HTMLElement{
     }
 }
 
-customElements.define("x-ayan", AyanElement);
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>
+        `;
+    }
+}
+
+customElements.define("x-center", CenterElement);
+
+
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-fullyear", YearElement);
